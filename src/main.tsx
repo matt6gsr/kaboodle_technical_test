11c86d689c24423561fc90226a309441e17eecfd
matt6gsr <matt6gsr@gmail.com>
2024-03-29 14:57:10 +0000

    configure redux store and route provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./pages/router";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { accommodationReducer } from "./redux/reducers/accommodation";
+import { Reducer } from "redux";
+import { PayloadType } from "./types/accommodation";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+const store = configureStore({
+  reducer: accommodationReducer as Reducer<PayloadType>,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
+
+export type AppDispatch = typeof store.dispatch;
